d689334bc689f6f61f724b32484016e65fa0274d
alexburdelev <alexbur228@gmail.com>
2023-04-17 07:15:57 +0300

    create QrCodeAnalyzer.kt

--- feature-scanner/src/main/java/ru/alexbur/feature/scanner/presentation/ScannerScreen.kt
+++ feature-scanner/src/main/java/ru/alexbur/feature/scanner/presentation/ScannerScreen.kt
@@ -1,18 +1,121 @@
 package ru.alexbur.feature.scanner.presentation
 
-import androidx.compose.runtime.Composable
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import ru.alexbur.core.di.navigation.NavigationFactory
 import ru.alexbur.core.di.navigation.NavigationScreenFactory
+import ru.alexbur.feature.scanner.di.ScannerComponent
+import ru.alexbur.feature.scanner.presentation.analyzer.QRCodeAnalyzer
 import javax.inject.Inject
 
 @Composable
 fun ScannerScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: ScannerViewModel = viewModel(modelClass = ScannerViewModel::class.java,
+        key = null,
+        factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return ScannerComponent.getComponent().getViewModel() as T
+            }
+        })
 ) {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+    var code by remember { mutableStateOf("") }
+    var hasCameraPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { granted -> hasCameraPermission = granted }
+    )
+
+    LaunchedEffect(key1 = true) {
+        launcher.launch(Manifest.permission.CAMERA)
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        if (hasCameraPermission) {
+            AndroidView(
+                factory = { context ->
+                    val previewView = PreviewView(context)
+                    val preview = Preview.Builder().build()
+                    val selector = CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
+                    preview.setSurfaceProvider(previewView.surfaceProvider)
+                    val imageAnalysis = ImageAnalysis.Builder()
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build()
+                    imageAnalysis.setAnalyzer(
+                        ContextCompat.getMainExecutor(context),
+                        QRCodeAnalyzer { result ->
+                            result?.let {
+                                code = it
+                                viewModel.scanBarcode(it)
+                            }
+                        }
+                    )
+
+                    runCatching {
+                        cameraProviderFuture.get().bindToLifecycle(
+                            lifecycleOwner,
+                            selector,
+                            preview,
+                            imageAnalysis
+                        )
+                    }
+
+                    return@AndroidView previewView
+                },
+                modifier = Modifier.weight(1f)
+            )
+            Text(
+                text = code,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp)
+            )
+        }
+    }
 }
 
 class ScannerScreenFactory @Inject constructor() : NavigationScreenFactory {
--- feature-scanner/src/main/java/ru/alexbur/feature/scanner/presentation/ScannerViewModel.kt
+++ feature-scanner/src/main/java/ru/alexbur/feature/scanner/presentation/ScannerViewModel.kt
@@ -1,9 +1,32 @@
 package ru.alexbur.feature.scanner.presentation
 
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.alexbur.core.domain.error_handler.ErrorHandler
+import ru.alexbur.core.domain.providers.StringProvider
 import ru.alexbur.core.presentation.BaseViewModel
+import ru.alexbur.core.presentation.snackbar.SnackBarStatus
+import ru.alexbur.feature.scanner.R
 import ru.alexbur.feature.scanner.domain.interactor.ScannerInteractor
 import javax.inject.Inject
 
 class ScannerViewModel @Inject constructor(
-    private val interactor: ScannerInteractor
-) : BaseViewModel<ScannerViewState>(ScannerViewState.Initial)
+    private val interactor: ScannerInteractor,
+    private val errorHandler: ErrorHandler,
+    private val stringProvider: StringProvider
+) : BaseViewModel<ScannerViewState>(ScannerViewState.Initial) {
+
+    fun scanBarcode(barcode: String) {
+        viewModelScope.launch {
+            if (barcode.isBlank()) {
+                // showSnackBar()
+                return@launch
+            }
+            interactor.inventory(barcode).onSuccess {
+                showSnackBar(stringProvider.getString(R.string.inventory_success, barcode), SnackBarStatus.SUCCESS)
+            }.onFailure {
+                showSnackBar(errorHandler.handleError(it), SnackBarStatus.ERROR)
+            }
+        }
+    }
+}
